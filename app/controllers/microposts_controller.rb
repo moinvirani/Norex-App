10547666fff8260a150cff36aa556091faad8ce4
Moin Virani <moin.virani@gmail.com>
2014-01-22 19:18:45 -0500

    microposts newsfeed complete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,22 +1,35 @@
 class MicropostsController < ApplicationController
   # before_action :signed_in_user
+  before_filter :load_micropost, :only => [:destroy]
 
-  def create
+   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
-  def destroy
+   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
-  private
 
+  private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+
+    def load_micropost
+      @micropost = Micropost.find(params[:id])
     end
 end
